6f3c43670559509ea417b0bc9a6f13aab05eb4e5
Sabbir <sabbir@hellotask.app>
2021-02-02 14:53:44 +0600

    Initial Commit

--- lib/facebook_event.dart
+++ lib/facebook_event.dart
@@ -1,7 +1,14 @@
 library facebook_event;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/services.dart';
+
+class FacebookEvent {
+  static const MethodChannel _channel =
+      const MethodChannel('facebook_event');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
 }
